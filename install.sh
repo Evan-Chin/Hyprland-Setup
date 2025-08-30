3ade1467ad511a8468abd8729c82b416e715da0c
Evan Chin <evanchin2025@hotmail.com>
2025-08-30 13:09:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,15 +28,15 @@ rm -rv ~/.config/starship.toml
 rm ~/.bashrc
 
 cd Hyprland-Setup
-mv -v ghostty ~/.config
-mv -v hypr ~/.config
-mv -v swaync ~/.config
-mv -v waybar ~/.config
-mv -v wlogout ~/.config
-mv -v wofi ~/.config
-mv -v starship.toml ~/.config
-mv -v bashrc ~/.bashrc
-mv -v wallpaper ~/
+cp -v ghostty ~/.config
+cp -v hypr ~/.config
+cp -v swaync ~/.config
+cp -v waybar ~/.config
+cp -v wlogout ~/.config
+cp -v wofi ~/.config
+cp -v starship.toml ~/.config
+mv bashrc ~/.bashrc
+cp wallpaper ~/
 
 echo Installation is done! Please log out and log back in!
 cd
